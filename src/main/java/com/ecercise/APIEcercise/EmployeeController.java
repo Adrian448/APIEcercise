fa462e836c9aea53aacbc1aefdf369ba54f30991
Adrian Rudolf <a-rudolf@outlook.de>
2020-04-02 21:54:14 +0200

    Update EmployeeController.java

--- src/main/java/com/ecercise/APIEcercise/EmployeeController.java
+++ src/main/java/com/ecercise/APIEcercise/EmployeeController.java
@@ -1,20 +1,15 @@
 package com.ecercise.APIEcercise;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.ecercise.entitys.Employee;
-import com.ecercise.restservice.Employees;
 
 @RestController
 @RequestMapping(path = "/Employees")
